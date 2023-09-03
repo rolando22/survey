583f7cdb1fd8115d628f355da109d03c38d2309d
rolando22 <rolando2207@gmail.com>
2023-09-02 21:03:51 -0300

    [ADD] TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 
 export function App() {
     return (
-        <></>
+        <>
+            <h1 className="text-3xl font-bold underline">Encuesta</h1>
+        </>
     );
 }
